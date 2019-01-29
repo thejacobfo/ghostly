f8c6d3154610bd904e008fb8c0a7aef72a605ed1
Jacob Folstrom <thejacobfo@gmail.com>
2019-01-29 11:08:58 -0500

    first

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Jacob Folstrom
-# Snowstorm (Snow Fall Effect) brought to you by http://www.schillmania.com/projects/snowstorm/
+# Snowstorm (Snow Fall Effect) brought to you by http://www.schillmania.com/projects/snowstorm/ // Redistributed under this Copyright license http://www.schillmania.com/projects/snowstorm/license.txt
 # the anime pic is from a series called "Serial Experiments" and the character in the image is named "Lain"
 # Pikachu and PokeBall Cursor brought to you from http://www.rw-designer.com
+# PAUSE/PLAY AUDIO W/ SPACEBAR
--- scripts/music.js
+++ scripts/music.js
@@ -1,10 +1,30 @@
-const player = document.getElementById('player')
-function controlAudio(){
-if (play = true) {
-player.pause()
-play = false }
-else { 
-    player.play()
-    play = true
+// const player = document.getElementById('player')
+// function(e){
+//     e.preventDefault
+// if (play = true) {
+// player.pause()
+// play = false }
+// else { 
+//     player.play()
+//     play = true
+// }
+// }
+var play = false;
+var myAudio = document.getElementById("player");
+
+function onKeyDown(event) {
+        switch (event.keyCode) {
+            case 32: //SpaceBar                    
+                if (play) {
+                    myAudio.pause();
+                    play = false;
+            
+                } else {
+                    myAudio.play(); 
+                    play = true;
+                }
+                break;
+        }
+  return false;
 }
-}
+window.addEventListener("keydown", onKeyDown, false);
